fa444b9a93972cc99b6f066f0d1cec1b9c2abbcb
niickmerc <nickdmercuri@gmail.com>
2022-03-26 13:33:06 -0700

    fixed API request

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -4,31 +4,22 @@ import yelp from "yelp-fusion";
 const apiKey = '6xRLOT2hzxA37NUpkb0rsyv92fqMhNv8O_c-2_PC04Ryt--xS5xQUQru2A8orO8EojnhSO5mjsUyFUvGFr0MSosv6b4FDMItXK60QMwRmqb7U4yMZ4M_Et_NOTU_YnYx';
 const clientId = 'k0nxX4jW8f0f45dVAD5_tQ';
 
-
-var locationInput = '2366 Main Mall,  Vancouver, BC, Canada';
-var categoryInput = 'Chinese';
-var radiusInput = 1000;  // in metres (1000/2000/5000/10000)
-
-const searchRequest = {
-    terms: categoryInput,
-    location: locationInput,
-    radius: radiusInput, 
-    open_now: true
-  };
-
 export const handleHome = (req, res) => {
     res.sendFile(path.join(__dirname+'/../home.html'));
 }
+
 
 // receives input from html form and passes it to the get request 
 export const handlePost = async (req, res) => {
     const {body : {terms, location, radius}} = req;
 
     const searchRequest = {
-        terms,
-        location,
+        categories: "restaurants, All",
+        term: terms,
+        location: location,
         radius: parseInt(radius), 
-        open_now: true
+        open_now: true,
+        limit: 5
       };
 
       // display search parameters on console
@@ -41,8 +32,7 @@ export const handlePost = async (req, res) => {
       const firstResult = await response.jsonBody.businesses[randomResult]
       const restaurantURL = await genGoogleLink(firstResult.coordinates.latitude, firstResult.coordinates.longitude, firstResult.name);
       
-    
-      console.log(firstResult);
+      console.log(response.jsonBody)
       console.log(restaurantURL);
 
     // pass x coord, y coord, and name string
