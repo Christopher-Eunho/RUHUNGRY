341855af045f0686e342075d4b5857766aaa2340
niickmerc <nickdmercuri@gmail.com>
2022-03-26 17:48:03 -0700

    final commit

--- src/app.js
+++ src/app.js
@@ -19,40 +19,35 @@ form.addEventListener('submit', async function(e) { // Add event listener (submi
     // Grab query value
     const searchedQuery = form.elements.locationQuery.value; 
     console.log(`Query: ${searchedQuery}`);
-    // Get request via API using Axios:
-    const results = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchedQuery}`); // <== TEST API
-    
-    
-    console.log(results);
- 
-    if (results.data.length === 0) { // check if there are any results to display
-        console.log("No result to display");
-        const msg = document.createElement("h2");
-        msg.innerText = "No result to display."   
-        queriedContainer.append(msg);
-    } else {
-        createImages(results);
-    }
+
+    // if (results.data.length === 0) { // check if there are any results to display
+    //     console.log("No result to display");
+    //     const msg = document.createElement("h2");
+    //     msg.innerText = "No result to display."   
+    //     queriedContainer.append(msg);
+    // } else {
+    //     createImages(results);
+    // }
     
 })
 
 // createImages
 // MODIFIES: document
 // EFFECTS: takes the queried results and displays the image of each result as an HTML element
-function createImages(results) {
-    const queriedShows = results.data;
-    for (let result of queriedShows) {
-        createImg(result);
-    }
-}
+// function createImages(results) {
+//     const queriedShows = results.data;
+//     for (let result of queriedShows) {
+//         createImg(result);
+//     }
+// }
 
 // createImg
 // MODIFIES: document
 // EFFECTS: takes a single result and displays the image as an HTML element
-function createImg(result) {
-    const img = document.createElement("img");
-        img.src = result.show.image.medium;
-        img.classList.add("mx-5", "my-5");
-        queriedContainer.append(img); // <== append to queryBox container
-}
+// function createImg(result) {
+//     const img = document.createElement("img");
+//         img.src = result.show.image.medium;
+//         img.classList.add("mx-5", "my-5");
+//         queriedContainer.append(img); // <== append to queryBox container
+// }
 
